11718ddd67e612d23fd1d305c10979002869f1ce
Harriss <harissitthikorn@gmail.com>
2023-10-05 16:33:35 +0700

    05/10/66

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connection = require('./connection');
 const userRoute = require('./routes/user');
+const prdRoute = require('./routes/product');
 const cors = require('cors');
 const app = express();
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
-app.use('/user',userRoute);
+app.use('/user', userRoute);
+app.use('/product', prdRoute);
 
 module.exports = app;
---
+++ routes/product.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const connection = require('../connection');
+const router = express.Router();
+
+
+router.post('/create', (req, res, next) => {
+    let prd = req.body;
+    query = "INSERT INTO product (prd_name,prd_price,prd_qty) VALUES (?,?,?)";
+    connection.query(query, [prd.prd_name, prd.prd_price, prd.prd_qty], (err, results) => {
+        if (!err) {
+            return res.status(200).json({ message: "เพิ่มข้อมูล product สำเร็จ" });
+        } else {
+            return res.status(500).json(err);
+        }
+    });
+});
+
+router.get('/read', (req, res, next) => {
+    query = "SELECT * FROM product";
+    connection.query(query, (err, results) => {
+        if (!err) {
+            return res.status(200).json(results);
+        } else {
+            return res.status(500).json(err);
+        }
+    });
+});
+
+router.get('/read/:id', (req, res, next) => {
+    const id = req.params.id;
+    query = "SELECT * FROM product WHERE prd_id = ?";
+    connection.query(query, [id], (err, results) => {
+        if (!err) {
+            if (results.length === 0) {
+                return res.status(404).json({ message: "ไม่พบข้อมูลที่ค้นหา" });
+            }
+            return res.status(200).json(results);
+        } else {
+            return res.status(500).json(err);
+        }
+    });
+});
+
+router.put('/update/:id', (req, res, next) => {
+    const id = req.params.id;
+    let prd = req.body;
+    query = "UPDATE product SET prd_name = ?, prd_price = ?, prd_qty = ? WHERE prd_id = ?";
+    connection.query(query, [prd.prd_name, prd.prd_price, prd.prd_qty, id], (err, results) => {
+        if (!err) {
+            if (results.length === 0) {
+                return res.status(404).json({ message: "ไม่พบ id" });
+            }
+            return res.status(200).json({ message: "แก้ไขข้อมูล product สำเร็จ" });
+        } else {
+            return res.status(500).json(err);
+        }
+    });
+});
+
+router.delete('/delete/:id', (req, res, next) => {
+    const id = req.params.id;
+    query = "DELETE FROM product WHERE prd_id = ?";
+    connection.query(query, [id], (err, results) => {
+        if (!err) {
+            if (results.length === 0) {
+                return res.status(404).json({ message: "ไม่พบ id" });
+            }
+            return res.status(200).json({ message: "ลบข้อมูล product สำเร็จ" });
+        } else {
+            return res.status(500).json(err);
+        }
+    })
+})
+
+
+module.exports = router;
