eec482995809d3db35b2719f46a43f9ed0104ffb
Harriss <harissitthikorn@gmail.com>
2023-10-06 15:24:50 +0700

    06/10/66

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connection = require('./connection');
 const userRoute = require('./routes/user');
 const prdRoute = require('./routes/product');
+const saleRoute = require('./routes/sale');
 const cors = require('cors');
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/user', userRoute);
 app.use('/product', prdRoute);
+app.use('/sale', saleRoute);
 
 module.exports = app;
